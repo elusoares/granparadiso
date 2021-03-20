1c73ebe5dd01e0713b76fa40362a209ef2454a12
Elusai Soares <elusaisoares@gmail.com>
2021-03-20 14:56:51 -0300

    landpage deployables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModulesModule } from './modules/modules.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModulesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
