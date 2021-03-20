869089dccd0ea9071842ab7a1705c3a629077a10
Elusai Soares <elusaisoares@gmail.com>
2021-03-20 14:55:31 -0300

    landpage deployables

---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ModulesRoutingModule } from './modules-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ModulesRoutingModule
+  ]
+})
+export class ModulesModule { }
