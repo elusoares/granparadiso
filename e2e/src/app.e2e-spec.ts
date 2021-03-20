5b37ca19df0f2e7c88ed5d7a33c228fe1cf08bc9
Elusai Soares <elusaisoares@gmail.com>
2021-03-20 13:01:03 -0300

    cors policy in index

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to granParadiso!');
+    expect(page.getTitleText()).toEqual('Welcome to gran-paradiso!');
   });
 
   afterEach(async () => {
